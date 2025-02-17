b9a6956124f00648219adabc4bb829a44d9d07ef
JBCargo <jezom094@uottawa.ca>
2025-02-16 20:15:33 -0500

    projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServicesPageComponent } from './services-page/services-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { LocationPageComponent } from './location-page/location-page.component';
+import { BookingPageComponent } from './booking-page/booking-page.component';
+import { ConfirmPageComponent } from './confirm-page/confirm-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'services', component: ServicesPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'location', component: LocationPageComponent },
+  { path: 'booking', component: BookingPageComponent },
+  { path: 'confirmation', component: ConfirmPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesPageComponent } from './services-page/services-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { LocationPageComponent } from './location-page/location-page.component';
+import { BookingPageComponent } from './booking-page/booking-page.component';
+import { ConfirmPageComponent } from './confirm-page/confirm-page.component';
+import { RouterLinkActive } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesPageComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    LocationPageComponent,
+    BookingPageComponent,
+    ConfirmPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterLinkActive],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
